30bc376f56a6be2695bca1c44184860bbccd6696
dino731 <leeain317@gmail.com>
2023-10-10 20:47:35 +0900

    first commit Ain 20231010

---
+++ firstJava/src/Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// ctrl + shift + L 단축키 전체 목록 보기
+		// ctrl + +, - 폰트 사이즈 크기 조절
+		// ctrl + D 한 줄 삭제
+		// ctrl + alt +down 행 단위 복사
+		// alt + shift + A 멀티 컬럼 편집
+		// alt + up, down 행 단위 이동
+		// ctrl + i 자동 들여쓰기
+		// ctrl + / 주석(토글)
+		// ctrl + shift + / 여러줄 주석
+		// ctrl + space 자동완성
+		//테스트
+		
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+
+
+	}
+
+}
